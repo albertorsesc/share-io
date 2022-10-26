7fa1ca5763a1c94246ce5b903f8d66b917317ddb
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-10-25 20:02:25 -0700

    Update Footer data

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -38,5 +38,5 @@ export const topics = [
 ];
 
 export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
-export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList2 = [ 'ShareIO for Good','Advertise','Developers','Transparency','ShareIO Rewards' ]
 export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
